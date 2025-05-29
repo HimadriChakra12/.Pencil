2900a387114956d3632580bb1c1c3917d97e220a
Himadri Chakrabarty <107918769+HimadriChakra12@users.noreply.github.com>
2025-05-29 18:09:25 +0600

    Update erase.ps1

--- sketches/erase.ps1
+++ sketches/erase.ps1
@@ -1,8 +1,8 @@
 $graphites = @(
-    @{ Name = "Neovim"; Path = "C:\Users\himadri\AppData\Local\nvim"; get = "nvim"},
-    @{ Name = "Github Cli"; Path = "C:\Users\himadri\AppData\Roaming\GitHub CLI"; get = "Github Cli"},
-    @{ Name = "Lazygit"; Path = "C:\Users\himadri\AppData\Local\lazygit"; get = "lazygit"},
-    @{ Name = "Sumatra PDF"; Path = "C:\Users\himadri\AppData\Local\SumatraPDF"; get = "SumatraPDF"}
+    @{ Name = "Neovim"; Path = "~\AppData\Local\nvim"; get = "nvim"},
+    @{ Name = "Github Cli"; Path = "~\AppData\Roaming\GitHub CLI"; get = "Github Cli"},
+    @{ Name = "Lazygit"; Path = "~\AppData\Local\lazygit"; get = "lazygit"},
+    @{ Name = "Sumatra PDF"; Path = "~\AppData\Local\SumatraPDF"; get = "SumatraPDF"}
 )
 
 foreach ($graphite in $graphites){
