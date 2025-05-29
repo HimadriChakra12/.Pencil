4b5bc8a8cac9e4cf9122f1443fed5d9e613367a5
Himadri Chakrabarty <107918769+HimadriChakra12@users.noreply.github.com>
2025-05-29 20:45:51 +0600

    Update factory.ps1

--- builder/factory.ps1
+++ builder/factory.ps1
@@ -3,7 +3,7 @@ if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdent
     [Security.Principal.WindowsBuiltInRole] "Administrator")) {
     #if not it will run the command on admin
     Write-Warning "Running this script as Administrator!"
-    Start-Process powershell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -Command "iwr -useb "https://tinyurl.com/hpencil" | iex ; exit "' -Verb RunAs
+    Start-Process powershell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -Command "iwr -useb "https://tinyurl.com/hpencil" | iex "' -Verb RunAs
     exit
 }
 Write-host "
