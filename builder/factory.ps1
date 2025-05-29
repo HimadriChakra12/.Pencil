430d0149064dda4820224378eca1ac6267e3b86c
HimadriChakra12 <himadrichakrabortydip@gmail.com>
2025-05-29 21:17:16 +0600

    HimUtil

--- builder/factory.ps1
+++ builder/factory.ps1
@@ -29,22 +29,3 @@ if(test-path $pencil){
 } else{
     write-host "Make a .graphite[windows dotfiles] repo with git and clone it" -ForegroundColor green
 }
-write-host "
-Looking For Charcoals.....
-=========================="  -ForegroundColor cyan
-$graphites = @(
-    @{ Name = "Neovim"; Path = "C:\Users\himadri\AppData\Local\nvim"; get = "nvim"},
-    @{ Name = "Github Cli"; Path = "C:\Users\himadri\AppData\Roaming\GitHub CLI"; get = "Github Cli"},
-    @{ Name = "Lazygit"; Path = "C:\Users\himadri\AppData\Local\lazygit"; get = "lazygit"},
-    @{ Name = "Sumatra PDF"; Path = "C:\Users\himadri\AppData\Local\SumatraPDF"; get = "SumatraPDF"}
-)
-
-foreach ($graphite in $graphites){
-    if(test-path $($graphite.Path)) {
-        write-host "Charcoal Found of '$($graphite.Name)' at [$($graphite.Path)]" -ForegroundColor green 
-    } else {
-        Write-Host "No Charcoal of $($graphite.Name): $_" -ForegroundColor Red
-    }
-}
- 
-
