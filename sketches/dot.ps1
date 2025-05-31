a483dc23fcdf52603258f2cd417fd2f81a9e830b
HimadriChakra12 <himadrichakrabortydip@gmail.com>
2025-05-31 07:10:55 -0700

    pencil

--- sketches/dot.ps1
+++ sketches/dot.ps1
@@ -4,11 +4,12 @@ $graphites = @(
     @{ Name = "Github Cli"; Path = "~\AppData\Roaming\GitHub CLI"; get = "Github Cli" },
     @{ Name = "Lazygit"; Path = "~\AppData\Local\lazygit"; get = "lazygit" },
     @{ Name = "Sumatra PDF"; Path = "~\AppData\Local\SumatraPDF"; get = "SumatraPDF" }
-    @{ Name = "scoop"; Path = "~\.config\scoop"; get = "scoop" }
-    @{ Name = "qimgv"; Path = "C:\Program Files\qimgv\conf"; get = "qimgv" }
-    @{ Name = "Glaze"; Path = "~\.glzr"; get = "glaze" }
-    @{ Name = "pwshprofile"; Path = "~\Documents\PowerShell"; get = "pwsh" }
-    @{ Name = "psprofile"; Path = "~\Documents\WindowsPowerShell"; get = "ps" }
+    @{ Name = "yasb"; Path = "~\.config\yasb"; get = "yasb" },
+    @{ Name = "scoop"; Path = "~\.config\scoop"; get = "scoop" },
+    @{ Name = "qimgv"; Path = "C:\Program Files\qimgv\conf"; get = "qimgv" },
+    @{ Name = "Glaze"; Path = "~\.glzr"; get = "glaze" },
+    @{ Name = "pwshprofile"; Path = "~\Documents\PowerShell"; get = "pwsh" },
+    @{ Name = "psprofile"; Path = "~\Documents\WindowsPowerShell"; get = "ps" },
     @{ Name = "Windows Terminal"; Path = "~\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\"; get = "terminal" }
 )
 
