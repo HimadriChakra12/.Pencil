a28f6a79b2d87aa53d4d6d4ff4f29fe2f29d0afc
HimadriChakra12 <himadrichakrabortydip@gmail.com>
2025-05-31 09:08:18 -0700

    Cleaned Graphites

--- sketches/erase.ps1
+++ sketches/erase.ps1
@@ -1,15 +1,3 @@
-#$graphites = @(
-    #@{ Name = "Neovim"; Path = "~\AppData\Local\nvim"; get = "nvim" },
-    #@{ Name = "Github Cli"; Path = "~\AppData\Roaming\GitHub CLI"; get = "Github Cli" },
-    #@{ Name = "Sumatra PDF"; Path = "~\AppData\Local\SumatraPDF"; get = "SumatraPDF" }
-    #@{ Name = "yasb"; Path = "~\.config\yasb"; get = "yasb" },
-    #@{ Name = "scoop"; Path = "~\.config\scoop"; get = "scoop" },
-    #@{ Name = "qimgv"; Path = "C:\Program Files\qimgv\conf"; get = "qimgv" },
-    #@{ Name = "Glaze"; Path = "~\.glzr"; get = "glaze" },
-    #@{ Name = "pwshprofile"; Path = "~\Documents\PowerShell"; get = "pwsh" },
-    #@{ Name = "psprofile"; Path = "~\Documents\WindowsPowerShell"; get = "ps" },
-    #@{ Name = "Windows Terminal"; Path = "~\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\"; get = "terminal" }
-#)
 Invoke-Expression (iwr "https://raw.githubusercontent.com/HimadriChakra12/.Pencil/master/graphites.ps1").Content
 
 foreach ($graphite in $graphites){
