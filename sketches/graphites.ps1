4c7b242e3a4eca8204d5098702b2014e75e54285
HimadriChakra12 <himadrichakrabortydip@gmail.com>
2025-05-31 08:53:03 -0700

    sketches

---
+++ sketches/graphites.ps1
@@ -0,0 +1,14 @@
+# .Pencil/graphites.ps1
+$Graphites = @(
+    @{ Name = "Neovim"; Path = "$env:USERPROFILE\AppData\Local\nvim"; Get = "nvim" },
+    @{ Name = "Github Cli"; Path = "$env:USERPROFILE\AppData\Roaming\GitHub CLI"; Get = "Github Cli" },
+    @{ Name = "Sumatra PDF"; Path = "$env:USERPROFILE\AppData\Local\SumatraPDF"; Get = "SumatraPDF" },
+    @{ Name = "yasb"; Path = "$env:USERPROFILE\.config\yasb"; Get = "yasb" },
+    @{ Name = "scoop"; Path = "$env:USERPROFILE\.config\scoop"; Get = "scoop" },
+    @{ Name = "qimgv"; Path = "C:\Program Files\qimgv\conf"; Get = "qimgv" },
+    @{ Name = "Glaze"; Path = "$env:USERPROFILE\.glzr"; Get = "glaze" },
+    @{ Name = "pwshprofile"; Path = "$env:USERPROFILE\Documents\PowerShell"; Get = "pwsh" },
+    @{ Name = "psprofile"; Path = "$env:USERPROFILE\Documents\WindowsPowerShell"; Get = "ps" },
+    @{ Name = "Windows Terminal"; Path = "$env:USERPROFILE\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\"; Get = "terminal" }
+)
+
