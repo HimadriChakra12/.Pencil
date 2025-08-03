e21bbfbe0c481d389c6d9741aacf7dc227c1ac00
HimadriChakra12 <himadrichakrabortydip@gmail.com>
2025-08-03 12:05:48 -0700

    Added State

--- graphites.ps1
+++ graphites.ps1
@@ -15,6 +15,7 @@ $Graphites = @(
     @{ Name = "Winget"; Path = "$env:USERPROFILE\AppData\Local\Packages\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe\LocalState\settings.json"; Get = "Winget/settings.json"; dir = $true; Got = "Winget" }
     @{ Name = "Visual Studio Code"; Path = "$env:USERPROFILE\AppData\Roaming\Code\User\settings.json"; Get = "Code/settings.json"; dir = $true; Got = "Code" }
     @{ Name = "Winstate"; Path = "$env:USERPROFILE\winstate.json"; Get = "wry/winstate.json"; dir = $true; Got = "wry" }
+    @{ Name = "State"; Path = "$env:USERPROFILE\state"; Get = "State"; dir = $false; Got = "State" }
     @{ Name = "Swiss Package"; Path = "C:\farm\wheats\Swissknife\package.json"; Get = "Swissknife\package.json"; dir = $true; Got = "Swissknife" }
     @{ Name = "jd2 cfg"; Path = "C:\Program Files\JDownloader\cfg"; Get = "JDownloader2"; dir = $false; Got = "JDownloader2" }
     @{ Name = "mpv"; Path = "$env:USERPROFILE\AppData\Roaming\mpv"; Get = "mpv"; dir = $false; Got = "mpv" }
